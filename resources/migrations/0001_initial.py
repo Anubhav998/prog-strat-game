33f298860ea8853d6f1a78dda556240516abb273
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2015-05-17 16:04:23 -0400

    stabalizing build

---
+++ resources/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Resource',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
+                ('description', models.TextField(blank=True)),
+            ],
+            options={
+                'verbose_name': 'resource',
+                'verbose_name_plural': 'resources',
+            },
+        ),
+    ]
