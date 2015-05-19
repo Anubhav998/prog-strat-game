72454129bff97fcd5094232281b5f185b3de535b
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2015-05-18 22:17:07 -0400

    code cleanup for military views

--- military/views.py
+++ military/views.py
@@ -1,14 +1,10 @@
-import json
-
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from military.models import Unit, Category
 from military.serializers import UnitSerializer, CategorySerializer
-from resources.models import Resource
 
 
 class UnitViewSet(viewsets.ModelViewSet):
