3fcc1b3299c39d4f2a3120ec61a9bb496777e544
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2015-05-22 12:44:07 -0400

    pylint'd pretty_print util

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,11 @@
 import pprint
 
 
-def prettyprint(output):
+def pretty_print(output):
+    """pretty prints an output
+
+    :param output: any python structure
+    :return: string
+    """
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(output)
